20b21a02b5134ba2e009ab4d1111ae46416872ff
Dietrich Rordorf <dr@ediqo.com>
2018-04-10 15:49:42 +0200

    add pma to docker composer setup

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,19 +1,18 @@
 #!/bin/bash
 set -e
 
-#echo "Installing"
-#retry "bin/setup system:install \
-#    --email='$ADMIN_EMAIL' \
-#    --password='$ADMIN_PASSWORD' \
-#    --db-host='$DB_HOST' \
-#    --db-user='$DB_USER' \
-#    --db-password='$DB_PASSWORD' \
-#    --db-template=en \
-#    --databox='$DB_DATA_NAME' \
-#    --appbox='$DB_APP_NAME' \
-#     --server-name='$WEB_HOST' \
-#     --yes"
-
+echo "Installing"
+retry "bin/setup system:install \
+    --email='$ADMIN_EMAIL' \
+    --password='$ADMIN_PASSWORD' \
+    --db-host='$DB_HOST' \
+    --db-user='$DB_USER' \
+    --db-password='$DB_PASSWORD' \
+    --db-template=en \
+    --databox='$DB_DATA_NAME' \
+    --appbox='$DB_APP_NAME' \
+     --server-name='$WEB_HOST' \
+     --yes"
 
 mkdir -p ./datas/phraseanet_data/documents
 
